93cf8e92ecaa7e94ede4874a6453d258d3b8c485
Marcel Würsch <marcel.wuersch@unifr.ch>
2017-06-28 14:24:59 +0200

    commented ocropustrain.sh

--- ocropus_model_training/sources/ocropustrain.sh
+++ ocropus_model_training/sources/ocropustrain.sh
@@ -1,17 +1,19 @@
+# Retrieve command line parameters
 inputZip=$1
 lineheight=$2
 trainIterations=$3
 saveFreq=$4
 resultFile=$5
 
+# Perform Pre-Processing
 export PYTHONWARNINGS="ignore"
-
 chmod +x /data/plot_train_err.py
-
 unzip $inputZip -d train
 
+# Execute the method
 /ocropy/ocropus-rtrain -q -l $lineheight -N $trainIterations -F $saveFreq -c train/*.gt.txt -o trainedModel train/*.bin.png
 
+#Perform Post-Processing
 /data/plot_train_err.py *.pyrnn.gz
 
 
@@ -24,4 +26,5 @@ visContent="$(base64 trainedModel-error.png)"
 minModelContent="$(base64 $minModel)"
 lastModelContent="$(base64 $lastModel)"
 
+# Write output JSON
 echo "{\"output\":[{ \"file\": { \"mime-type\": \"image/png\", \"name\":\"trainingError.png\", \"options\":{ \"type\":\"color\", \"visualization\":true }, \"content\": \""$visContent"\" } },{ \"file\": { \"mime-type\": \"application/x-compressed\", \"name\":\"minModel.pyrnn.gz\", \"options\":{ \"type\":\"ocromodel\", \"visualization\":false, \"error\": \""$minError"\" }, \"content\": \""$minModelContent"\" } },{ \"file\": { \"mime-type\": \"application/x-compressed\", \"name\":\"lastModel.pyrnn.gz\", \"options\":{ \"type\":\"ocromodel\", \"visualization\":false, \"error\": \""$lastError"\" }, \"content\": \""$lastModelContent"\" } } ]}" > $resultFile
